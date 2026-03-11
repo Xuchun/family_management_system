5525d9625b1a6109c72a52a9bd8d9042edfc58d9
Xuchun <xuchunli@gmail.com>
2026-03-11 14:54:39 +0800

    UI: shorten input box width and add vertical_alignment=bottom to fix download button vertical offset

--- app.py
+++ app.py
@@ -565,7 +565,7 @@ try:
         st.session_state["temp_task_text"] = st.session_state.get("input_new_task", "")
         st.session_state["input_new_task"] = ""
 
-    col_add_input, col_add_btn, col_dl_btn = st.columns([0.60, 0.17, 0.23])
+    col_add_input, col_add_btn, col_dl_btn = st.columns([0.50, 0.20, 0.30], vertical_alignment="bottom")
     with col_add_input:
         st.text_input("➕ 新增事项:", placeholder="例如：每周二拿快递...", key="input_new_task", label_visibility="collapsed")
     with col_add_btn:
